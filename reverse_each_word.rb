201067f512e0511711ee7d9d6239e50bfe003cc6
Jack de Grave <jack.degrave@outlook.com>
2019-05-31 12:40:25 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,6 +7,14 @@ def reverse_each_word(string)
   return string
 end
 
+def reverse_each_word(string)
+  array = string.split(" ")
+  array.collect do |element|
+    element.reverse!
+  string = array.join(" ")
+  end
+  return string
+end
 
 #def uses_collect(string)
  #   array = string.split(" ")
