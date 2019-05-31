1c8eacf79b41bd85a1f26e4357cf542c4809f872
Jack de Grave <jack.degrave@outlook.com>
2019-05-31 12:36:50 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,17 @@
+def reverse_each_word(string)
+  array = string.split
+  array.each do |element|
+    element.reverse!
+  string = array.join(" ")
+  end
+  return string
+end
+
+def uses_collect(string)
+    array = string.split
+    new_array = []
+  array.collect do |element|
+    new_array << element.reverse!
+  end
+  return new_array.join(" ")
+end
